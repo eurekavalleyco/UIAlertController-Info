36c39f453a3a83ee65a2fe8903dfdf235e2aa819
Ken M. Haggerty <eurekavalleyco@gmail.com>
2015-04-10 10:22:55 -0400

    Adds files for UIAlertController+Info.h/.m

---
+++ UIAlertController+Info.h
@@ -0,0 +1,21 @@
+//
+//  UIAlertController+Info.h
+//  Databox
+//
+//  Created by Ken M. Haggerty on 1/3/15.
+//  Copyright (c) 2015 Eureka Valley Co. All rights reserved.
+//
+
+#pragma mark - // NOTES (Public) //
+
+#pragma mark - // IMPORTS (Public) //
+
+#import <UIKit/UIKit.h>
+
+#pragma mark - // PROTOCOLS //
+
+#pragma mark - // DEFINITIONS (Public) //
+
+@interface UIAlertController (Info)
+@property (nonatomic, strong) NSDictionary *info;
+@end
---
+++ UIAlertController+Info.m
@@ -0,0 +1,49 @@
+//
+//  UIAlertController+Info.m
+//  Databox
+//
+//  Created by Ken M. Haggerty on 1/3/15.
+//  Copyright (c) 2015 Eureka Valley Co. All rights reserved.
+//
+
+#pragma mark - // NOTES (Private) //
+
+#pragma mark - // IMPORTS (Private) //
+
+#import "UIAlertController+Info.h"
+#import "AKDebugger.h"
+#import <objc/runtime.h>
+
+#pragma mark - // DEFINITIONS (Private) //
+
+static char infoKey;
+
+@implementation UIAlertController (Info)
+
+#pragma mark - // SETTERS AND GETTERS //
+
+- (void)setInfo:(NSDictionary *)info
+{
+    [AKDebugger logMethod:METHOD_NAME logType:AKLogTypeMethodName methodType:AKMethodTypeSetup customCategory:nil message:nil];
+    
+    objc_setAssociatedObject(self, &infoKey, info, OBJC_ASSOCIATION_RETAIN_NONATOMIC);
+}
+
+- (NSDictionary *)info
+{
+    [AKDebugger logMethod:METHOD_NAME logType:AKLogTypeMethodName methodType:AKMethodTypeGetter customCategory:nil message:nil];
+    
+    return objc_getAssociatedObject(self, &infoKey);
+}
+
+#pragma mark - // INITS AND LOADS //
+
+#pragma mark - // PUBLIC METHODS //
+
+#pragma mark - // DELEGATED METHODS //
+
+#pragma mark - // OVERWRITTEN METHODS //
+
+#pragma mark - // PRIVATE METHODS //
+
+@end
