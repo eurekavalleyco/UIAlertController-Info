2669dd9bec9a092512a9f587438a1339be2e775f
Eureka Valley Co <eurekavalleyco@gmail.com>
2015-12-21 14:24:37 -0500

    Updates project reference.

--- UIAlertController+Info.h
+++ UIAlertController+Info.h
@@ -1,6 +1,6 @@
 //
 //  UIAlertController+Info.h
-//  Databox
+//  UIAlertController+Info
 //
 //  Created by Ken M. Haggerty on 1/3/15.
 //  Copyright (c) 2015 Eureka Valley Co. All rights reserved.
